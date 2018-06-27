33fb5f746f99fcf3417d453a3a64f68ffdda30cc
Daniel Young <dyoung@corporate.wasson-ece.com>
2018-06-27 09:56:56 -0600

    Used more appropriate class name.

--- src/Migrations/2018_06_27_011745_add_used_for_and_modified_part_translation.php
+++ src/Migrations/2018_06_27_011745_add_used_for_and_modified_part_translation.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddSoftDeleteToPartTranslation extends Migration
+class AddUsedForAndModifiedFieldsToPartTranslation extends Migration
 {
     /**
      * Run the migrations.
